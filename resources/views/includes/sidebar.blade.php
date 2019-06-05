4c7a7d18a6d433b35b43d125ea784260c3b31e7d
Kåre Andersen <kare.andersen@piql.com>
2019-06-05 10:07:44 +0200

    started work on sidebar nav

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,14 +1,14 @@
-<div class="sideMenu">
+<div class="wrapper sideMenu">
     <div id="welcomeText">
         Welcome<br><i class="fas fa-user"></i> {{ Auth::user()->full_name }}
     </div>
     <div id="collapseSideMenu">
-        <img onclick="collapseMenu()" src="images/collapsemenu.png">
+        <img onclick="collapseMenu()" src="{{asset('images/collapsemenu.png')}}">
     </div>
     <div class="clearboth"></div>
-    <ul>
-        <a href="/"><li><i class="fas fa-tachometer-alt"></i><i class="leftMenuItem"> Dashboard</i></li></a>
-        <a href="reports"><li><i class="fas fa-chart-bar"></i><i class="leftMenuItem"> Reports</i></li></a>
+    <ul class="list-group">
+        <a href="/"><li class="list-group-item sidebar-active active"><i class="fas fa-tachometer-alt"></i><i class="leftMenuItem"> Dashboard</i></li></a>
+        <a href="reports"><li class="list-group-item"><i class="fas fa-chart-bar"></i><i class="leftMenuItem"> Reports</i></li></a>
     </ul>
 
     <div id="poweredBy">Powered by </div>
