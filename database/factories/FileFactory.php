a38f3a84da6b464d42fd500af4c13b1ec99dde90
Simen Fjeld-Olsen <simen.fjeldolsen@piql.com>
2019-08-08 13:49:33 +0200

    Added filesize to File factory.

--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -12,35 +12,45 @@ $factory->define(File::class, function (Faker $faker) {
 	if ($chance < 34)
 	{
 		$extension = "pdf";
+		$size = rand(100000, 100000000); // Between 100KB and 100MB
 	} elseif ($chance < 54)
 	{
 		$extension = "tif";
+		$size = rand(10000000, 500000000); // Between 10MB and 500MB
 	} elseif ($chance < 69)
 	{
 		$extension = "jpg";
+		$size = rand(500000, 10000000); // Between 500KB and 10MB
 	} elseif ($chance < 79)
 	{
 		$extension = "xml";
+		$size = rand(1000, 10000000); // Between 1KB and 10MB
 	} elseif ($chance < 89)
 	{
 		$extension = "png";
+		$size = rand(500000, 10000000); // Between 500KB and 10MB
 	} elseif ($chance < 92)
 	{
 		$extension = "zip";
+		$size = rand(100000, 100000000000); // Between 100KB and 100GB
 	} elseif ($chance < 95)
 	{
 		$extension = "docx";
+		$size = rand(100000, 10000000); // Between 100KB and 10MB
 	} elseif ($chance < 97)
 	{
 		$extension = "avi";
+		$size = rand(10000000, 10000000000); // Between 10MB and 100GB
 	} else
 	{
 		$extension = "mp4";
+		$size = rand(10000000, 100000000000); // Between 10MB and 100GB
 	}
 
     return [
         'bag_id' => factory(App\Bag::class)->create(),
         'filename' => "test_" . $faker->numberBetween(0, 10000) . "." . $extension,
+        'filesize' => $size,
         'uuid' => $faker->uuid,
     ];
 });
