91124ea06487f1c3089cc887e013a427b013e3cc
Kåre Andersen <kare.andersen@piql.com>
2019-08-08 10:39:06 +0000

    more css improvements

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,9 +17,10 @@ mix.webpackConfig({
     },
 });
 
+mix.sass('resources/sass/app.scss', 'public/css');
+
 mix.js('resources/js/app.js', 'public/js')
     .js(['node_modules/fine-uploader-wrappers/azure.js', 'node_modules/fine-uploader-wrappers/base-wrapper.js', 'node_modules/fine-uploader-wrappers/callback-names.js', 'node_modules/fine-uploader-wrappers/callback-proxy.js', 'node_modules/fine-uploader-wrappers/traditional.js', 'node_modules/fine-uploader/fine-uploader/fine-uploader.js'], 'public/js/fine-uploader.js')
-    .sass('resources/sass/app.scss', 'public/css')
     .js('node_modules/bootstrap-select/dist/js/bootstrap-select.min.js', 'public/js')
     .styles('node_modules/bootstrap-select/dist/css/bootstrap-select.min.css', 'public/css/bootstrap-select.min.css')
     .js('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js', 'public/js')
