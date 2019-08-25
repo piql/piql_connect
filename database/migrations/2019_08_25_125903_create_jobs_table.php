4004ddf06821b91761c81e861f4fe052c9921f85
Kare Andersen <kareandersen@gmail.com>
2019-08-25 15:14:58 +0200

    restored database integrity

--- database/migrations/2019_08_25_125903_create_jobs_table.php
+++ database/migrations/2019_08_25_125903_create_jobs_table.php
@@ -15,11 +15,12 @@ class CreateJobsTable extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('uuid');
-            $table->string('name');
-            $table->string('status');
-            $table->string('owner');
-            $table->timestamps();
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
         });
     }
 
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -9,9 +9,9 @@ class UsersTableSeeder extends Seeder
     use Uuids;
 
     public function run()
-		{
-				$found = App\User::where('username', '=', 'simen')->first();
-				$user = $found ?? new App\User();
+    {
+        $found = App\User::where('username', '=', 'simen')->first();
+        $user = $found ?? new App\User();
         $user->username = "simen";
         $user->password = Hash::make("simen");
         $user->full_name = "Simen Fjeld-Olsen";
@@ -20,18 +20,16 @@ class UsersTableSeeder extends Seeder
         $user = App\User::findByUsername("simen");
         $user->id = '07da0453-857f-4645-92ac-9a3add6427cf';
         $user->save();
-				
-				$found = App\User::where('email', '=', 'kare.andersen@piql.com')->first();
-				$found->delete();
-				$found = App\User::where('username', '=', 'kare')->first();
-				$user = $found ?? new App\User();
-				$user = new App\User();
+
+        $found = App\User::where('username', '=', 'kare')->first();
+        $user = $found ?? new App\User();
+        $user = new App\User();
         $user->username = "kare";
         $user->password = Hash::make("kare");
         $user->full_name = "Kåre Andersen";
         $user->email = "kare.andersen@piql.com";
         $user->save();
-        
+
     }
 }
 //            'id' => '07da0453-857f-4645-92ac-9a3add6427cf',
