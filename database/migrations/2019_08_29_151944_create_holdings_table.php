9fe9e7800bb9555e22d03a6126688fb29b87910d
Kåre Andersen <kare.andersen@piql.com>
2019-08-29 18:19:46 +0200

    initial data structure for holdings and fonds

---
+++ database/migrations/2019_08_29_151944_create_holdings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHoldingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holdings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid');
+            $table->string('title')->unique();
+            $table->text('description')->nullable();
+            $table->uuid('parent_uuid')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holdings');
+    }
+}
