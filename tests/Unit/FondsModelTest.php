1537d753abc809f95ca8cac5e1e787f47c58e731
Kåre Andersen <kare.andersen@piql.com>
2019-09-01 21:46:35 +0200

    made fonds model test pass

--- tests/Unit/FondsModelTest.php
+++ tests/Unit/FondsModelTest.php
@@ -16,8 +16,9 @@ class FondsModelTest extends TestCase
      */
     public function test_it_creates_top_level_fonds_by_default()
     {
-        $fonds = Fonds::create();
+        $fonds = Fonds::create(['title' => 'top-level-fonds', 'owner_holding_uuid' => 'b0c7100d-2920-4340-a09c-e5ccee0fa90e']);
         $fonds->fresh();
         $this->assertTrue($fonds->ancestor() ==  null );
+        $fonds->delete();
     }
 }
