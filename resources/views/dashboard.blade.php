5d76e2dfcf2fbe8e6e6af2866381e0542d345897
Simen Fjeld-Olsen <simen.fjeldolsen@piql.com>
2019-09-16 15:45:29 +0200

    Color scheme to pink and other cosmetic changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
         <div class="col-sm-12">
             <div class="row">
                 <div class="col-sm-6">
-                    <!-- <h2 style="text-align: center;margin-bottom: 5px;">Ingest</h2> -->
+                    <h2 style="text-align: center;margin-bottom: 5px;">Ingest</h2>
                     <div>
                         <div class="row">
                             <div class="col-sm-6">
@@ -43,7 +43,7 @@
                     </div>
                 </div>
                 <div class="col-sm-6">
-                    <!-- <h2 style="text-align: center;margin-bottom: 5px;">Access</h2> -->
+                    <h2 style="text-align: center;margin-bottom: 5px;">Access</h2>
                     <div class="row">
                         <div class="col-sm-6">
                             {!! $monthlyOnlineAIPsAccessed->container() !!}
@@ -55,55 +55,55 @@
                         </div>
                     </div>
                     <div>
-                        <div class="row pt-3" style="justify-content: space-evenly;">
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
+                        <div class="row pt-3" style="justify-content: space-evenly;margin-top: 30px;">
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
                                     <strong>{{ $infoArray['onlineAIPsIngested'] }}</strong>
-                                </h1>
+                                </h2>
                                 <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">AIPs stored online</em>
                             </div>
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
                                     <strong>{{ $infoArray['onlineDataIngested'] }}</strong>
-                                </h1>
+                                </h2>
                                 <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Stored online</em>
                             </div>
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
-                                    <strong>{{ $infoArray['offlineReelsCount'] }}</strong>
-                                </h1>
-                                <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Something</em>
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
+                                    <strong>{{ $infoArray['AIPsRetrievedCount'] }}</strong>
+                                </h2>
+                                <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">AIPs retrieved</em>
                             </div>
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
-                                    <strong>{{ $infoArray['offlineDataIngested'] }}</strong>
-                                </h1>
-                                <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Something</em>
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
+                                    <strong>{{ $infoArray['DataRetrieved'] }}</strong>
+                                </h2>
+                                <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Retrieved</em>
                             </div>
                         </div>
-                        <div class="row pt-3" style="justify-content: space-evenly;">
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
+                        <div class="row pt-3" style="justify-content: space-evenly;margin-top: 20px">
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
                                     <strong>{{ $infoArray['offlineAIPsIngested'] }}</strong>
-                                </h1>
+                                </h2>
                                 <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">AIPs stored on film</em>
                             </div>
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
                                     <strong>{{ $infoArray['offlineDataIngested'] }}</strong>
-                                </h1>
+                                </h2>
                                 <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Stored on film</em>
                             </div>
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
                                     <strong>{{ $infoArray['offlineReelsCount'] }}</strong>
-                                </h1>
+                                </h2>
                                 <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Reels</em>
                             </div>
-                            <div class="card dashboard-card" class="col-sm-3" style="height:150px;width: 150px;">
-                                <h1 style="color: white;text-align: center;padding-top: 50px;font-size: 300%">
+                            <div class="card dashboard-card" class="col-sm-3" style="height:100px;width: 150px;">
+                                <h2 style="color: white;text-align: center;padding-top: 25px;font-size: 300%">
                                     <strong>{{ $infoArray['offlinePagesCount'] }}</strong>
-                                </h1>
+                                </h2>
                                 <em style="color: white;text-align: center;position: absolute;left: 0;right: 0;bottom: 0;">Pages stored on film</em>
                             </div>
                         </div>
