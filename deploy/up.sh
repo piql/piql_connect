60fcc7855e4252118d1052c8235ede217310d0e2
Probably Kåre or Simon <kare.andersen@piql.com>
2019-12-20 13:41:01 +0100

    Changed Piql Conenct config use external volumes towards Archivematica

--- deploy/up.sh
+++ deploy/up.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-export HOST_USERID=333
-export HOST_GROUPID=333
-sudo chown -R $HOST_USERID:$HOST_GROUPID /var/lib/docker/volumes
-sudo chown -R $HOST_USERID:$HOST_GROUPID /var/lib/docker/volumes
-sudo chmod -R 777 ../resources/
 docker-compose -p piqlConnect up --force-recreate -d
