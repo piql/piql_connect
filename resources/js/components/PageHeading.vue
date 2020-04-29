c13334290c566025f674736d2765e73c8c51550b
Kåre Andersen <kare.andersen@piql.com>
2020-04-29 19:00:28 +0200

    CON-390 Created page header component

---
+++ resources/js/components/PageHeading.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="pageHeader">
+        <div class="row titleRow">
+            <div class="d-inline-flex">
+                <i :class="icon" class="fas titleIcon"></i>
+                <span class="titleText">{{title}}</span>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col ingressText">{{ingress}}</div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: "as-hdd"
+        },
+        title: {
+            type: String,
+            default: "Page Title"
+        },
+        ingress: {
+            type: String,
+            default: "Page description"
+        }
+    },
+};
+</script>
