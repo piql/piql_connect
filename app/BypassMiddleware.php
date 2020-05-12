c3dddf2bd6d9a3a17ebb1790662354d7eb7a24c1
hakonlarsson <hakonlarsson@github.com>
2020-05-12 13:50:42 +0000

    CON-384 Fixed issue with ingesting files of unknown type

--- app/BypassMiddleware.php
+++ app/BypassMiddleware.php
@@ -24,15 +24,16 @@ class BypassMiddleware implements LayerInterface
     public function peel($object, Closure $next)
     {
 
-        if ($object->getType() !== 'image') {
-            return $next($object);
-        }
+        //if ($object->getType() !== 'image') {
+        //    return $next($object);
+        //}
 
         $edition = new Edition(self::editionKey, $object->getUploaderPath(), $object->getUploaderPath(), [
             'type' => 'file',
         ], true);
 
         $object->addEdition($edition);
+        $object->setType('file');
 
         return $next($object);
     }
