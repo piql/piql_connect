71a1f128b6c5e532e9e553ede45a9316800cb295
Kåre Andersen <kare.andersen@piql.com>
2020-05-13 13:25:56 +0200

    CON-452 fix missing includes to allow phpunit coverage

--- app/Http/Controllers/Api/Access/FileObjectController.php
+++ app/Http/Controllers/Api/Access/FileObjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Access;
 
+use App\Http\Controllers\Controller;
 use App\FileObject;
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/Api/Access/MetsController.php
+++ app/Http/Controllers/Api/Access/MetsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Access;
 
+use App\Http\Controllers\Controller;
 use App\Mets;
 use Illuminate\Http\Request;
 
