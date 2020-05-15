abb4524510c6000d35d29a57180647161d1b431c
Kåre Andersen <kare.andersen@piql.com>
2020-05-15 11:44:22 +0200

    CON-470 very basic list header component created

---
+++ resources/js/components/ListHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="row plistHeader text-truncate text-center mt-2">
+        <div v-show="!colA.hidden" :class="colA.css">{{colA.slot}}</div>
+        <div v-show="!colB.hidden" :class="colB.css">{{colB.slot}}</div>
+        <div v-show="!colC.hidden" :class="colC.css">{{colC.slot}}</div>
+        <div v-show="!colD.hidden" :class="colD.css" v-html="colD.slot" @click="btnDClicked"></div>
+    </div>
+</template>
+
+<script>
+//* //HACK: Very basic component for list headers.
+//  Needs refactoring to support arbitrary columns and actions,probably by using a list of slots.
+//*
+
+export default {
+    props: {
+        colA: {
+            type: Object,
+            default: () => {
+                return {
+                    css: "col-sm-2",
+                    slot: "column A",
+                    hidden: true
+                };
+            },
+        },
+        colB: {
+            type: Object,
+            default: () =>{
+                return {
+                    css: "col-sm-7 text-left",
+                    slot: "column B",
+                    hidden: true
+                };
+            }
+        },
+        colC: {
+            type: Object,
+            default: () => {
+                return {
+                    css: "col-sm-2 text-center",
+                    slot: "column C",
+                    hidden: true
+                };
+            }
+        },
+        colD: {
+            type: Object,
+            default: () => {
+                return {
+                    css: "col-sm-1",
+                    slot: "column D",
+                    hidden: true
+                };
+            }
+        }
+
+    },
+
+    methods: {
+        btnDClicked: function() { this.$emit('btnDClicked'); }
+    }
+};
+</script>
