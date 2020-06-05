95fb98ed7e1dc7c1e46161a6dacb2681464da43b
Kåre Andersen <kare.andersen@piql.com>
2020-06-05 12:23:29 +0200

    CON-488 spanish, catalan, br portoguese locales added

--- app/Http/Controllers/Api/System/SystemController.php
+++ app/Http/Controllers/Api/System/SystemController.php
@@ -10,7 +10,13 @@ class SystemController extends Controller
 {
     public function languages( Request $request )
     {
-        return collect([ ['id' => '0', 'title' => 'English', 'code' => 'en'], ['id' => '1', 'title' => 'Norsk Bokmål', 'code' => 'nb_no'] ]);
+        return collect([ 
+            ['id' => '0', 'title' => 'English', 'code' => 'en'],
+            ['id' => '1', 'title' => 'Català', 'code' => 'ca_es'],
+            ['id' => '2', 'title' => 'Español', 'code' => 'es_es'],
+            ['id' => '3', 'title' => 'Norsk Bokmål', 'code' => 'nb_no'],
+            ['id' => '4', 'title' => 'Português', 'code' => 'pt_br']
+        ]);
     }
 
     public function sessionLifetime( Request $request )
