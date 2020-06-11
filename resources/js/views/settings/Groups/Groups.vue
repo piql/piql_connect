225bf23441be7afee0089313a5c0a7446968c5e3
Steven Sewalu <steven.sewalu@contango.no>
2020-06-11 17:04:20 +0300

    user groups ui work

--- resources/js/views/settings/Groups/Groups.vue
+++ resources/js/views/settings/Groups/Groups.vue
@@ -1,20 +1,69 @@
 <template>
     <div class="w-100">
-        <h1>Groups</h1>
+        <page-heading icon="fa-users" :title="$t('settings.settings.groups')" :ingress="$t('settings.settings.groupsdesc')" />
+        <div class="card">
+            <div class="card-header">
+        
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addModal">
+                    <i class="fa fa-plus"></i>  Add Group
+                </button>
+            </div>
+            <div class="card-body">
+               <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModal" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        
+                        <form role="form" method="post">
+                            <fieldset>
+                                <div class="modal-body">
+                                    <legend>Add Group </legend>
+                                    <div class="form-group">
+                                        <label>Group</label>
+                                        <input type="text" class="form-control" v-model="group" >
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Description</label>
+                                        <textarea v-model="description" class="form-control"></textarea>
+                                    </div>
+                                   
+                                
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" @submit="addGroup" class="btn btn-primary"><i class="fa fa-plus"></i>Add Group</button>
+                                </div>
+                            </fieldset>
+
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         
     </div>
 </template>
 
 <script>
+
     export default {
+       
         data() {
             return {
-                userSettings: null
+                group:null,
+                description:null,
+                response: null
             };
         },
 
         methods: {
+            async addGroup(){
+                 this.response = (await axios.post("/api/v1/admin/permissions/groups", {
+                    name: this.group,
+                    description: this.description
+                })).data;
+            }
         }
     }
 </script>
+
