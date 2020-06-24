745484fb5fffd6dcdb202a003150db42bbf7a19c
Ronald Kasagga <ronald.kasagga@contango.no>
2020-06-24 09:48:20 +0300

    try setting readable ID

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,7 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            "id" => $this->id,
+            "id" => $this->getIdAttribute(),
             "username" => $this->username,
             "full_name" => $this->full_name,
             "email" => $this->email,
