a9856f9341ffcbeb29ddd8c7f3a77dc7f9183c98
hakonlarsson <hakonlarsson@github.com>
2020-07-30 06:54:36 +0000

    CON-506 resolved relationship but accessing owner will still return owner ID as tring and not owner as App\User. In the latter case we still have to use owner()->first() or owner()->getResultes()

--- app/Bag.php
+++ app/Bag.php
@@ -139,7 +139,7 @@ class Bag extends Model
 
     public function owner()
     {
-        return $this->hasOne('App\User', 'id', 'owner_id');
+        return $this->belongsTo('App\User', 'owner_id', 'id');
     }
 
     public function files()
