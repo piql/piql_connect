dbb70566290e0c9b49fb09f712fca4e9d23e0ef1
Patrick Givisiez <pgivisiez@gmail.com>
2020-08-05 16:11:01 -0300

    CON-100: Preview audio and videos on preview lightbox

---
+++ resources/js/components/lightbox/VideoPlayer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <video ref="videoPlayer" class="video-js"></video>
+    </div>
+</template>
+
+<script>
+import videojs from 'video.js';
+import 'video.js/dist/video-js.min.css';
+import 'video.js/dist/video.min.js';
+
+export default {
+    name: "VideoPlayer",
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    },
+    data() {
+        return {
+            player: null
+        }
+    },
+    mounted() {
+        this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
+            console.log('onPlayerReady', this);
+        })
+    },
+    beforeDestroy() {
+        if (this.player) {
+            this.player.dispose()
+        }
+    }
+}
+</script>
