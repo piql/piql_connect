90da4fd010db63d750bed912bd2267f08cc5f9dc
Ronald Kasagga <ronald.kasagga@contango.no>
2020-08-12 10:59:30 +0300

    swagger autogeneration setup

--- app/AccessControl.php
+++ app/AccessControl.php
@@ -3,10 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * Class AccessControl
+ * 
+ * @package App
+ * 
+ * @OA\Schema(
+ *     description="Holds data for Permissions, Roles, Permission",
+ *     title="AccessControl",
+ *     required={"name"}
+ * )
+*/
 class AccessControl extends Model
 {
-    //
+    public $name;
+    /**
+     * @OA\Property(
+     *     description="Access Control Name",
+     *     title="name",
+     *     format="string"
+     * )
+     */   
+    
+    public $description;
+     /**
+     * @OA\Property(
+     *     description="Access Control Description",
+     *     title="description",
+     *     format="string"
+     * )
+     */
 
     
 }
