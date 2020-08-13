7f76f41b28bc8014b4b55370b770f8018a0974b5
Ronald Kasagga <ronald.kasagga@contango.no>
2020-08-13 11:50:11 +0300

    syntax refactor

--- deploy/logstash/init.sh
+++ deploy/logstash/init.sh
@@ -3,10 +3,10 @@
 can_write_dir() {
   path=$1
   if [ ! -d $path ]; then
-    echo "$path does not exist"
+    echo "$path does not exist";
     return 1
   elif ! [[ -w "$path" ]] ; then
-    echo "Cannot write to '$path'"
+    echo "Cannot write to '$path'";
     return 1
   fi
   return 0
@@ -15,10 +15,10 @@ can_write_dir() {
 can_read_dir() {
   path=$1
   if [ ! -d $path ]; then
-    echo "$path does not exist"
+    echo "$path does not exist";
     return 1
   elif ! [[ -r "$path" ]] ; then
-    echo "Cannot write to '$path'"
+    echo "Cannot write to '$path'";
     return 1
   fi
   return 0
@@ -27,7 +27,7 @@ can_read_dir() {
 create_dir_if_not_exists() {
     path=$1
     if [ ! -d $path ]; then
-        echo "creating directory $path"
+        echo "creating directory $path";
         mkdir -p $path || return 1
     fi
     return 0
@@ -43,31 +43,31 @@ configure_ingest_templates() {
         return 1
     fi
 
-    echo "clean up old configuration files"
+    echo "clean up old configuration files";
     config_dir_files=($config_dir/*.conf)
     if [ ${#config_dir_files[@]} -gt 0 ]; then 
       rm $config_dir/*.conf
     fi
 
-    echo "load new configuration files"
+    echo "load new configuration files";
     cp $template_dir/*.conf $config_dir/
 
-    echo "sed: replacing configuration variables with .env parameters"
+    echo "sed: replacing configuration variables with .env parameters";
     for conf_path in $config_dir/*conf; do 
-        echo "processing $conf_path ..."
-        sed -ie "s/{{mysql_host}}/$MYSQL_HOST/g" $conf_path
-        sed -ie "s/{{mysql_port}}/$MYSQL_PORT/g" $conf_path
-        sed -ie "s/{{mysql_name}}/$MYSQL_NAME/g" $conf_path
-        sed -ie "s/{{mysql_user}}/$MYSQL_USER/g" $conf_path
-        sed -ie "s/{{mysql_pass}}/$MYSQL_PASS/g" $conf_path
-        sed -ie "s/{{mongo_user}}/$MONGO_USER/g" $conf_path
-        sed -ie "s/{{mongo_pass}}/$MONGO_PASS/g" $conf_path
-        sed -ie "s/{{mongo_dbnm}}/$MONGO_NAME/g" $conf_path
-        sed -ie "s/{{mongo_host}}/$MONGO_HOST/g" $conf_path
-        sed -ie "s/{{mongo_port}}/$MONGO_PORT/g" $conf_path
+        echo "processing $conf_path ...";
+        sed -ie "s/{{mysql_host}}/$MYSQL_HOST/g" $conf_path;
+        sed -ie "s/{{mysql_port}}/$MYSQL_PORT/g" $conf_path;
+        sed -ie "s/{{mysql_name}}/$MYSQL_NAME/g" $conf_path;
+        sed -ie "s/{{mysql_user}}/$MYSQL_USER/g" $conf_path;
+        sed -ie "s/{{mysql_pass}}/$MYSQL_PASS/g" $conf_path;
+        sed -ie "s/{{mongo_user}}/$MONGO_USER/g" $conf_path;
+        sed -ie "s/{{mongo_pass}}/$MONGO_PASS/g" $conf_path;
+        sed -ie "s/{{mongo_dbnm}}/$MONGO_NAME/g" $conf_path;
+        sed -ie "s/{{mongo_host}}/$MONGO_HOST/g" $conf_path;
+        sed -ie "s/{{mongo_port}}/$MONGO_PORT/g" $conf_path;
     done
 
-    echo "cleaning up after sed.."
+    echo "cleaning up after sed..";
     rm $config_dir/*.confe
 }
 
@@ -77,38 +77,31 @@ CONFIG_DIR=/tmp/logstash/conf.d
 TEMPLATES_DIR=/etc/logstash/config-templates
 CONFIG_PATH=/usr/share/logstash/config/pipelines.yml
 
-mkdir -p $CONFIG_DIR
+create_dir_if_not_exists $CONFIG_DIR
 
-echo ""
-echo "processing general configurations"
-# res=$(configure_ingest_templates $TEMPLATES_DIR $CONFIG_DIR)
-if [[ "$(configure_ingest_templates $TEMPLATES_DIR $CONFIG_DIR)" -eq 1 ]]; then 
-    exit 1
-fi
-
-echo ""
-echo "processing pipelined configurations"
-echo ""
+echo "";
+echo "processing pipelined configurations";
+echo "";
 plugins=("inputs" "outputs")
 for i in "${plugins[@]}"; do
     cnf_dir="$CONFIG_DIR/$i"
     tmp_dir="$TEMPLATES_DIR/$i"
     
-    echo ""
-    echo "processing pipelined $i configurations"
+    echo "";
+    echo "processing pipelined $i configurations";
     if [[ "$(create_dir_if_not_exists $cnf_dir)" -eq 1 ]]; then 
         exit 1
     elif [[ "$(configure_ingest_templates $tmp_dir $cnf_dir)" -eq 1 ]]; then 
         exit 1
     fi
-    echo "completed processing pipelined $i configurations"
+    echo "completed processing pipelined $i configurations";
 done
 
 #todo: check if plugin exists first before redownloading
 
-echo "installing plugins"
+echo "installing plugins";
 logstash-plugin install --version=3.1.5 logstash-output-mongodb 
 
 #start logstash with configuration folder
-echo "starting logstash with pipeline config: '$CONFIG_PATH'"
+echo "starting logstash with pipeline config: '$CONFIG_PATH'";
 logstash 
