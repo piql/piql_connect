a22b9dece501e423c92d5355b82f5cac317dae58
Kåre Andersen <kare.andersen@piql.com>
2020-08-20 10:53:34 +0200

    CON-629 built view with dynamic metadata entry form

--- resources/js/components/DublinCoreTemplate.vue
+++ resources/js/components/DublinCoreTemplate.vue
@@ -1,33 +1,34 @@
 <template>
-    <div class="w-100">
-        <form>
-            <div class="row">
-                <div v-for="scheme in schemes" class="col-sm-4">
-                    <div v-for="schemeItem in scheme" class="row mb-2">
-                        <div class="col-sm-7 form-group mb-2">
-                            <label class="small" >{{schemeItem.label}}</label>
-                            <input class="form-control input-sm" type="text"
-                                v-bind:readOnly=readOnly
-                                v-bind:id="schemeItem.name"
-                                v-bind:value="getValue(schemeItem.name)"
-                                @input="setValue(schemeItem.name, $event.target.value)">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <div v-if="!readOnly" class="row mt-5">
-                <div class="col-6 mr-4"></div>
-                <button class="btn btn mr-2 pr-5 pl-5 col-2" @click="$router.go(-1)">{{$t('Cancel')}}</button>
-                <button class="btn btn-ln btn-default pr-5 pl-5 col-2" @click="save()">{{$t('OK')}}</button>
-            </div>
-            <div v-if="readOnly" class="row mt-5">
-                <div class="col-8 mr-4"></div>
-                <button class="col-2 pl-3 pr-3 text-center btn btn-ln btn-default" @click="$router.go(-1)">{{$t('Back')}}</button>
-            </div>
-
-        </div>
-    </template>
+    <b-modal id="meta" size="xl" :centered=true 
+        :title="$t('admin.metadata.template.edit.title')"
+        :header-class="['d-ruby','text-center']"
+        :no-fade=true title-class="h3"
+        :ok-title="$t('admin.metadata.template.edit.saveButton')"
+        :cancel-title="$t('admin.metadata.template.edit.cancelButton')"
+    >
+        <b-container fluid>
+            <b-row v-for="(scheme,idx) in schemes" :key="scheme.id">
+                <b-col class="text-center">
+                    <h4>{{$t('admin.metadata.template.edit.schemeName')}}:&nbsp;{{scheme.type}}</h4>
+                </b-col>
+                <b-form inline>
+                    <b-col class="form-group">
+                        <b-form-row v-for="schemeItem in scheme.fields" :key="schemeItem.id" class="w-100">
+                            <label :for="fieldId(scheme.type, schemeItem.name)" class="small" >{{schemeItem.label}}</label>
+                            <b-col class="m-2 w-100">
+                                <b-form-input class="w-100"
+                                    :id="fieldId(scheme.type, schemeItem.name)"
+                                    v-model="metadataObject.metadata.dc[schemeItem.name]"
+                                    v-bind:readOnly=readOnly
+                                />
+                            </b-col>
+                        </b-form-row>
+                    </b-col>
+                </b-form>
+            </b-row>
+        </b-container>
+    </b-modal>
+</template>
 
 <script>
 import axios from 'axios';
@@ -35,97 +36,73 @@ import axios from 'axios';
 export default {
     data () {
         return {
-            metadataObject: {
-                metadata: [{ dc : {} }]
-            }
+            metadataObject: { "metadata": {"dc": {}} }
         }
     },
 
     props: {
         schemes: {
             type: Array,
-            default : () => [
-                [
-                    {"name" : "title",       "label" : "Title",       "type": "text"},
-                    {"name" : "creator",     "label" : "Creator",     "type": "text"},
-                    {"name" : "subject",     "label" : "Subject",     "type": "text"},
-                    {"name" : "description", "label" : "Description", "type": "text"},
-                    {"name" : "publisher",   "label" : "Publisher",   "type": "text"},
-                ], [
-                    {"name" : "contributor", "label" : "Contributor", "type": "text"},
-                    {"name" : "date",        "label" : "Date",        "type": "text"},
-                    {"name" : "type",        "label" : "Type",        "type": "text"},
-                    {"name" : "format",      "label" : "Format",      "type": "text"},
-                    {"name" : "identifier",  "label" : "Identifier",  "type": "text"},
-                ], [
-                    {"name" : "source",      "label" : "Source",      "type": "text"},
-                    {"name" : "language",    "label" : "Language",    "type": "text"},
-                    {"name" : "relation",    "label" : "Relation",    "type": "text"},
-                    {"name" : "coverage",    "label" : "Coverage",    "type": "text"},
-                    {"name" : "rights",      "label" : "Rights",      "type": "text"},
-                ]
-            ]
+            default : () => {
+                return [
+                    {
+                        "type": "Dublin Core v1.1",
+                        "fields":
+                        [
+                            {"name" : "title",       "label" : "Title",       "type": "text"},
+                            {"name" : "creator",     "label" : "Creator",     "type": "text"},
+                            {"name" : "subject",     "label" : "Subject",     "type": "text"},
+                            {"name" : "description", "label" : "Description", "type": "text"},
+                            {"name" : "publisher",   "label" : "Publisher",   "type": "text"},
+                            {"name" : "contributor", "label" : "Contributor", "type": "text"},
+                            {"name" : "date",        "label" : "Date",        "type": "text"},
+                            {"name" : "type",        "label" : "Type",        "type": "text"},
+                            {"name" : "format",      "label" : "Format",      "type": "text"},
+                            {"name" : "identifier",  "label" : "Identifier",  "type": "text"},
+                            {"name" : "source",      "label" : "Source",      "type": "text"},
+                            {"name" : "language",    "label" : "Language",    "type": "text"},
+                            {"name" : "relation",    "label" : "Relation",    "type": "text"},
+                            {"name" : "coverage",    "label" : "Coverage",    "type": "text"},
+                            {"name" : "rights",      "label" : "Rights",      "type": "text"},
+                        ]
+                    }
+                ];
+            },
         },
         readOnly: {
             type: Boolean,
             default: false
-        }
+        },
+        initialTemplate: {
+            type: Object,
+            default: null
+        },
     },
 
     computed: {
-        idParam: function() {
-            return this.$route.params[this.urlParam];
-        },
-        url: function() {
-            return `${this.baseUrl}/${this.idParam}/metadata`;
-        },
-        fileName: function() {
-            if (!this.file && this.baseFileUrl && this.metadataObject && this.idParam) {
-                let urlTmp = this.baseFileUrl + this.idParam;
-                axios.get(urlTmp).then( async ( resp ) =>  {
-                    this.file = resp.data;
-                });
-            }
-            if (this.file) {
-                return this.file.filename;
-            } else {
-                return "";
-            }
-        }
     },
-    async mounted () {
+    mounted () {
     },
-
+    watch: {
+        initialTemplate: function(value){
+            this.metadataObject.metadata = JSON.parse(JSON.stringify(value.metadata)); //deep copy template
+        },
+    },
     methods: {
         getValue(key) {
-            if((this.metadataObject.metadata.dc !== undefined) &&
-                (key in this.metadataObject.metadata.dc))
-                return this.metadataObject.metadata.dc[key];
-            else
-                return "";
+            return (((this.metadataObject||{}).metadata||{}).dc||{})[key]
+                ? this.metadataObject.metadata.dc[key]
+                : "";
         },
         setValue(key, value) {
+            if(! ((this.metadataObject||{}).metadata||{}).dc ) {
+                this.metadataObject = { "metadata": {"dc": {} } };
+            }
             this.metadataObject.metadata.dc[key] = value;
         },
-        async save() {
-            if(!this.readOnly) {
-                // save errorToast handler just in case there is an error
-                // because this object may not be present when the error occur
-                let errorToast = this.errorToast;
-                this.patch(
-                    this.url + '/' + this.metadataObject.id,
-                    this.metadataObject,
-                ).catch(function (error) {
-                    // todo: proper error handling and field input validation
-                    let title = "Metadata error";
-                    let message = (error.errors === undefined) ? "Saving matadata failed" : JSON.stringify(error.errors);
-                    errorToast(title, message);
-                });
-
-                this.$router.go(-1);
-            }
-        },
-
+        fieldId(type, name){
+            return `${type}-${name}`.replace(/\s/g,''); /* Strip all whitespace from fieldId */
+        }
     }
 };
 </script>
---
+++ resources/js/components/MetadataTemplateList.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <table class="table table-hover table-sm table-bordered table-vcenter text-truncate">
+            <thead>
+                <tr>
+                    <th class="text-center">{{$t("admin.account.metadata.title")}}</th>
+                    <th class="text-center">{{$t("admin.account.metadata.identifier")}}</th>
+                    <th class="text-center">{{$t("admin.account.metadata.created_at")}}</th>
+                    <th class="text-center">{{$t("admin.account.metadata.actions")}}</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="template in metadataTemplateFix" :key="template.id" class="mt-2">
+                    <td class="text-center">{{template.metadata.dc.title}}</td>
+                    <td class="text-center">{{template.metadata.dc.identifier}}</td>
+                    <td class="text-center">{{template.created_at}}</td>
+
+                    <td class="align-self-center text-center">
+                        <a class="ml-2 mr-2 btn btn-xs text-white"
+                            @click="cloneTemplate(template)" :title="$t('admin.account.metadata.template.clone')" >
+                            <i class="fas fa-clone"></i></a>
+                        <a class="ml-2 mr-2 btn btn-xs text-white"
+                            @click="assignTemplate(template)" :title="$t('admin.account.metadata.template.assign')" >
+                            <i class="fas fa-paperclip"></i></a>
+
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+
+    mixins: [],
+
+    data () {
+        return {
+        }
+    },
+
+    props: {
+    },
+
+    computed: {
+        metadataTemplateFix: function() {
+            return [
+                {"id":79, "created_at": "2020-05-22", "metadata":{"dc":{"title":"Svalbard","creator":"a photographer","subject":"cold things","description":"brrr","publisher":"","contributor":"","date":"","type":"","format":"","identifier":"63ff83ef-862e-428f-9cc3-2040693d493e","source":"","language":"","relation":"","coverage":"","rights":""}}},
+                {"id":80, "created_at": "2020-05-23", "metadata":{"dc":{"title":"Drammen","creator":"that guy","subject":"awesome place","description":"yay","publisher":"","contributor":"","date":"","type":"","format":"","identifier":"63ff83ef-862e-428f-9cc3-2040693d493e","source":"","language":"","relation":"","coverage":"","rights":""}}}
+            ];
+        }
+    },
+
+    created () {
+    },
+
+    mounted () {
+    },
+
+    methods: {
+        cloneTemplate: function( template ) {
+            this.$emit('cloneTemplate', template);
+        },
+    }
+};
+</script>
