edcce8ed269e982e6445b817aa061686605b57a5
Kåre Andersen <kare.andersen@piql.com>
2020-08-25 13:12:03 +0200

    CON-633 templating now talks to its vuex store

--- resources/js/components/MetadataTemplateList.vue
+++ resources/js/components/MetadataTemplateList.vue
@@ -3,27 +3,28 @@
         <table class="table table-hover table-sm table-bordered table-vcenter text-truncate">
             <thead>
                 <tr>
-                    <th class="text-center">{{$t("admin.account.metadata.title")}}</th>
-                    <th class="text-center">{{$t("admin.account.metadata.identifier")}}</th>
-                    <th class="text-center">{{$t("admin.account.metadata.created_at")}}</th>
+                    <th class="text-center p-3">{{$t("admin.account.metadata.title")}}</th>
+                    <th class="text-center p-3">{{$t("admin.account.metadata.creator")}}</th>
+                    <th class="text-center p-3">{{$t("admin.account.metadata.created_at")}}</th>
+                    <th class="text-center p-3">{{$t("admin.account.metadata.subject")}}</th>
                     <th class="text-center">{{$t("admin.account.metadata.actions")}}</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr v-for="template in metadataTemplateFix" :key="template.id" class="mt-2">
-                    <td class="text-center">{{template.metadata.dc.title}}</td>
-                    <td class="text-center">{{template.metadata.dc.identifier}}</td>
-                    <td class="text-center">{{template.created_at}}</td>
+                <tr v-for="template in metadataTemplates" :key="template.id" class="mt-2">
+                    <td class="text-left p-3">{{template.metadata.dc.title}}</td>
+                    <td class="text-left p-3">{{template.metadata.dc.creator}}</td>
+                    <td class="text-center p-3">{{template.created_at}}</td>
+                    <td class="text-left p-3">{{template.metadata.dc.subject}}</td>
 
                     <td class="align-self-center text-center">
                         <a class="ml-2 mr-2 btn btn-xs text-white"
-                            @click="cloneTemplate(template)" :title="$t('admin.account.metadata.template.clone')" >
+                            @click="cloneTemplate(template.id)" :title="$t('admin.account.metadata.template.clone')" >
                             <i class="fas fa-clone"></i></a>
                         <a class="ml-2 mr-2 btn btn-xs text-white"
                             @click="assignTemplate(template)" :title="$t('admin.account.metadata.template.assign')" >
                             <i class="fas fa-paperclip"></i></a>
-
                     </td>
                 </tr>
 
@@ -44,15 +45,13 @@ export default {
     },
 
     props: {
+        metadataTemplates: {
+            type: Array,
+            default: null
+        }
     },
 
     computed: {
-        metadataTemplateFix: function() {
-            return [
-                {"id":79, "created_at": "2020-05-22", "metadata":{"dc":{"title":"Svalbard","creator":"a photographer","subject":"cold things","description":"brrr","publisher":"","contributor":"","date":"","type":"","format":"","identifier":"63ff83ef-862e-428f-9cc3-2040693d493e","source":"","language":"","relation":"","coverage":"","rights":""}}},
-                {"id":80, "created_at": "2020-05-23", "metadata":{"dc":{"title":"Drammen","creator":"that guy","subject":"awesome place","description":"yay","publisher":"","contributor":"","date":"","type":"","format":"","identifier":"63ff83ef-862e-428f-9cc3-2040693d493e","source":"","language":"","relation":"","coverage":"","rights":""}}}
-            ];
-        }
     },
 
     created () {
@@ -62,8 +61,8 @@ export default {
     },
 
     methods: {
-        cloneTemplate: function( template ) {
-            this.$emit('cloneTemplate', template);
+        cloneTemplate: function( id ){
+            this.$emit('cloneTemplate', id);
         },
     }
 };
