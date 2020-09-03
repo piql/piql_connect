f086a1b986d9fe5cb8f3410b699b3ce66659d734
Patrick Givisiez <pgivisiez@gmail.com>
2020-09-03 19:35:54 -0300

    CON-687: Performing suggested changes

--- app/Helpers/MetadataHelper.php
+++ app/Helpers/MetadataHelper.php
@@ -3,17 +3,12 @@
 namespace App\Helpers;
 
 class MetadataHelper {
-    static function csvEscape($value, $enclosure='"', $delimiter=',', $escapeChar="\\") {
-        $value = str_replace($enclosure, $enclosure . $enclosure, $value);
-        if (strchr($value, $delimiter) !== false ||
-            strchr($value, $enclosure) !== false ||
-            strchr($value, $escapeChar) !== false ||
-            strchr($value, "\n") !== false ||
-            strchr($value, "\r") !== false ||
-            strchr($value, "\t") !== false ||
-            strchr($value, ' ') !== false) {
-                return $enclosure . $value . $enclosure;
-        }
+    static function csvEscape($value) {
+        $file = fopen('php://memory', 'w+');
+        fputcsv($file, array($value));
+        rewind($file);
+        $value = preg_replace("/[\r\n]+/", '', stream_get_contents($file));
+        fclose($file);
         return $value;
     }
 }
