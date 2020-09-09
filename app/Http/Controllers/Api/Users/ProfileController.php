e158ec932c70822c240f0cb14e2ae38e1faa6a23
Patrick Givisiez <pgivisiez@gmail.com>
2020-09-09 19:45:58 -0300

    CON-688: Add an endpoint to upload a profile picture

--- app/Http/Controllers/Api/Users/ProfileController.php
+++ app/Http/Controllers/Api/Users/ProfileController.php
@@ -11,19 +11,19 @@ class ProfileController extends Controller {
     const PROFILE_IMAGE_PATH = 'custom-files/user/';
 
     public function imgUpload(Request $request) {
-    	$user = Auth::user();
+        $user = Auth::user();
         $src = storage_path('uploader/profileImg/'.$request->result["name"]);
-    	$dst = storage_path(self::PROFILE_IMAGE_PATH . $user->id . '.png');
+        $dst = storage_path(self::PROFILE_IMAGE_PATH . $user->id . '.png');
         rename($src, $dst);
         return response()->json(['success' => true]);
     }
 
     public function img() {
-    	$user = Auth::user();
-    	$file = self::PROFILE_IMAGE_PATH . $user->id . '.png';
-    	if (!file_exists(storage_path($file))) {
-    		$file = self::PROFILE_IMAGE_PATH . 'avatar.png';
-    	}
-    	return response()->file(storage_path($file));
-	}
+        $user = Auth::user();
+        $file = self::PROFILE_IMAGE_PATH . $user->id . '.png';
+        if (!file_exists(storage_path($file))) {
+            $file = self::PROFILE_IMAGE_PATH . 'avatar.png';
+        }
+        return response()->file(storage_path($file));
+    }
 }
