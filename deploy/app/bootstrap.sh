acf436e84d226397bbc9922f09d1014509b81740
Simon Bruce-Cassidy <simon.brucecassidy@piql.com>
2020-10-09 08:41:49 +0000

    CON-830 Removed steps from bootstrap.sh

--- deploy/app/bootstrap.sh
+++ deploy/app/bootstrap.sh
@@ -4,10 +4,10 @@ cd /var/www
 #usermod -u 333 -g 333 www-data
 chmod 777 ./storage/logs/*
 ZSH=/var/www/.zsh sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended 
-php artisan telescope:publish
-php artisan migrate
-php artisan db:seed
-php artisan vue-i18n:generate
-php artisan passport:keys
+#php artisan telescope:publish
+#php artisan migrate
+#php artisan db:seed
+#php artisan vue-i18n:generate
+#php artisan passport:keys
 /usr/bin/supervisord
 /usr/local/sbin/php-fpm
