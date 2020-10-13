7a1c8b5c0b046e85caeebeed3097b5774cb3c490
Patrick Givisiez <pgivisiez@gmail.com>
2020-10-13 20:38:26 -0300

    CON-813: improving tests

--- tests/Feature/DipControllerTest.php
+++ tests/Feature/DipControllerTest.php
@@ -5,6 +5,7 @@ use App\User;
 use Tests\TestCase;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Str;
+use App\FileObject;
 
 class DipControllerTest extends TestCase
 {	
@@ -17,7 +18,11 @@ class DipControllerTest extends TestCase
 	
 	public function test_show_file_returns_200()
     {
-        $response = $this->get('/api/v1/access/dips/files/123');
+    	$fileObject = factory(FileObject::class)->create(['fullpath'=>'xxxx', 'filename'=>'xxxxx', 'path'=>'xxxx', 'size'=>123, 'object_type'=>'file', 'info_source'=>'connect', 'mime_type'=>'text/xml', 'storable_type'=>'App\Dip', 'storable_id'=>1]);
+    	$response = $this->get('/api/v1/access/dips/files/' . $fileObject->id);
         $response->assertOk();
+        $resp = $response->decodeResponseJson();
+        $this->assertEquals($fileObject->id, $resp['id']);
+        $fileObject->delete();
     }
 }
