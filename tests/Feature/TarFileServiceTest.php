b575c0207fe7087d1b8351929cc86ff51df71810
Patrick Givisiez <pgivisiez@gmail.com>
2020-10-28 20:43:40 -0300

    CON-843: PR changes

--- tests/Feature/TarFileServiceTest.php
+++ tests/Feature/TarFileServiceTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 use App\Services\TarFileService;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+use splitbrain\PHPArchive\Tar;
 
 class TarFileServiceTest extends TestCase
 {
@@ -14,28 +16,44 @@ class TarFileServiceTest extends TestCase
     }
     public function test_tar_big_filenames_multiple_files()
     {
-        $testPath = '/tmp/' . time();
-        mkdir($testPath);
-        $bigFileName = $testPath . '/' . str_repeat('a', 240) . '.txt';
-        $tarFile = '/tmp/' . time() . '.tar';
-        file_put_contents($bigFileName,'test');
-        $this->tarFileService->collectMultipleFiles(array(basename($bigFileName) => $bigFileName), $tarFile, false);
+        $bigFileName = str_repeat('a', 240) . '.txt';
+        $storage = Storage::fake('tar1');
+        $storage->put($bigFileName, "Hello world");
+        $tarFile = $storage->path(time() . '.tar');
+        $bigFileNamePath = $storage->path($bigFileName);
+        $this->tarFileService->collectMultipleFiles(array(basename($bigFileName) => $bigFileNamePath), $tarFile, false);
         $this->assertEquals(true, file_exists($tarFile));
-        unlink($bigFileName);
-        rmdir($testPath);
+        $tar = new Tar();
+        $tar->open($tarFile);
+        $outPath = $storage->path('out');
+        mkdir($outPath);
+        $tar->extract($outPath);
+        $this->assertEquals(true, file_exists($outPath . '/' . $bigFileName));
+        unlink($bigFileNamePath);
         unlink($tarFile);
     }
+
     public function test_tar_big_filenames_directory()
     {
-        $testPath = '/tmp/' . time();
-        mkdir($testPath);
-        $bigFileName = $testPath . '/' . str_repeat('a', 240) . '.txt';
-        $tarFile = '/tmp/' . time() . '.tar';
-        file_put_contents($bigFileName,'test');
+        $bigFileName = str_repeat('a', 240) . '.txt';
+        $pathTmp = 'tmp/';
+        $storage = Storage::fake('tar2');
+        $storage->put($pathTmp . $bigFileName, "Hello world");
+        $testPath = $storage->path($pathTmp);
+        $bigFileName = str_repeat('a', 240) . '.txt';
+        $bigFileNamePath = $testPath . str_repeat('a', 240) . '.txt';
+        $tarFile = $storage->path(time() . '.tar');
+        $storage->put($bigFileNamePath, "fake file");
         $this->tarFileService->collectDirectory($testPath, $tarFile);
         $this->assertEquals(true, file_exists($tarFile));
-        unlink($bigFileName);
-        rmdir($testPath);
+        $tar = new Tar();
+        $tar->open($tarFile);
+        $outPath = $storage->path('out');
+        mkdir($outPath);
+        $tar->extract($outPath);
+        $this->assertEquals(true, file_exists($outPath . '/' . $bigFileName));
+        unlink($bigFileNamePath);
         unlink($tarFile);
     }
+
 }
