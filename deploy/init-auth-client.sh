da801dc360e71f498dc3cbb4f4c9bba2097bed6d
Steven Sewalu <steven.sewalu@contango.no>
2020-11-06 12:30:03 +0300

    build script update for environment.js

--- deploy/init-auth-client.sh
+++ deploy/init-auth-client.sh
@@ -8,10 +8,12 @@ fi
 realm=${AUTH_REALM:-'development'}
 url=${AUTH_BASE_URL:-'https://auth.piqlconnect.com/auth/'}
 clientId=${AUTH_CLIENT:-'piql-connect-frontend'}
+testUrl=${APP_FE_URL:-'http://localhost/'}
 
 sed -e "s|process.env.AUTH_REALM|'$realm'|g" \
     -e "s|process.env.AUTH_BASE_URL|'$url'|g" \
     -e "s|process.env.AUTH_CLIENT|'$clientId'|g" \
+    -e "s|process.env.APP_FE_URL|'$testUrl'|g" \
     $current_dir/../resources/js/environment.js.template >\
     $current_dir/../resources/js/environment.js || exit $?
 
