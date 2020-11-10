b50edf3b7001bba7ce9eefe582f64af8ecae6fb2
Matti Wennberg <matti.wennberg@piql.com>
2020-11-10 17:27:39 +0100

    CON-908 Use fake keycloak client to be able to actually test the service functions

--- tests/Unit/KeycloakClientTest.php
+++ tests/Unit/KeycloakClientTest.php
@@ -4,25 +4,72 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Mockery;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use App\Interfaces\KeycloakClientInterface;
 use App\Services\KeycloakClientService;
 use App\User;
 
+class FakeKeycloakClient
+{
+    private $users;
+
+    public function __construct($users)
+    {
+        $this->users = $users;
+    }
+
+    public function getUsers()
+    {
+        return $this->users->map( function ($user) {
+            return [
+                'id' => $user->id,
+                'username' => $user->username,
+                'password' => $user->password,
+                'full_name' => $user->full_name,
+                'email' => $user->email,
+                'account_uuid' => $user->account_uuid ];
+        })->toArray();
+    }
+}
+
 class KeycloakClientTest extends TestCase
 {
-    private $user;
+    private $users;
+    private $service;
+    private $client;
 
     protected function setUp() : void
     {
         parent::setUp();
 
-        $this->user = new User([
-            'username' => 'kctest',
-            'password' => 'kctestpass',
-            'full_name' => 'Key Cloaksson',
-            'email' => 'kc@piql.com',
-            'account_uuid' => '1d860602-130e-4624-99ad-d532e1b662ab'
-        ]);
+        // Create test users
+        $this->users = collect([
+            new User([
+                'username' => 'kctest1',
+                'password' => 'kctestpass1',
+                'full_name' => 'Key Cloaksson',
+                'email' => 'kc1@piql.com',
+                'account_uuid' => '1d860602-130e-4624-99ad-d532e1b662ab']),
+            new User([
+                'username' => 'kctest2',
+                'password' => 'kctestpass2',
+                'full_name' => 'Key Cloaksson Sr.',
+                'email' => 'kc2@piql.com',
+                'account_uuid' => '98dd00df-afba-4031-8dd1-18c09a29acbd']),
+            new User([
+                'username' => 'kctest3',
+                'password' => 'kctestpass3',
+                'full_name' => 'Key Cloaksson Jr.',
+                'email' => 'kc3@piql.com',
+                'account_uuid' => '46e9d918-270b-43fe-b3a8-4e919e28a05c']) ]);
+        foreach ($this->users as $user) {
+            $user->setIdAttribute(Str::uuid());
+        }
+
+        // Create service with fake client
+        $this->client = new FakeKeycloakClient($this->users);
+        $this->service = $this->app->makeWith('App\Services\KeycloakClientService', ['app' => $this->app, 'keycloakClient' => $this->client]);
     }
 
     public function test_when_injecting_an_instance_of_KeycloakClientInterface_it_makes_a_KeycloakClientService()
@@ -33,15 +80,9 @@ class KeycloakClientTest extends TestCase
 
     public function test_when_requesting_users_a_user_list_is_returned()
     {
-        $this->instance( KeycloakClientInterface::class, Mockery::mock(
-            KeycloakClientService::class, function ( $mock ) {
-                $mock->shouldReceive( 'getUsers' )->times(1);
-            } )
-        );
-
-        $service = $this->app->make( KeycloakClientInterface::class );
-
-        $users = $service->getUsers();
+        $users = $this->service->getUsers();
+        $this->assertEquals($users, $this->client->getUsers());
+        $this->assertEquals(count($users), $this->users->count());
     }
 
     public function test_when_adding_a_user_the_user_is_added()
