3f7c1aa3cb2f6807c7f5fb89ffbcdeaf26647086
Ronald Kasagga <ronald.kasagga@contango.no>
2020-11-18 12:16:16 +0300

    wip: role and group management

--- app/Interfaces/KeycloakClientInterface.php
+++ app/Interfaces/KeycloakClientInterface.php
@@ -3,29 +3,33 @@
 
 namespace App\Interfaces;
 
+use App\Auth\Group;
 use App\User;
 
 interface KeycloakClientInterface
 {
+    /**
+     * @return []User
+    */
     public function getUsers() : array;
     public function createUser(string $organizationId, User $user) : User;
     public function editUser(string $userId, User $user) : User;
-    public function deleteUser(string $organizationId) : void;
+    public function deleteUser(string $userId) : void;
 
     public function getUserById($id);
     public function searchOrganizationUsers($orgId, $params=[], $limit=20, $offset=0);
 
-    // public function getRoles(): array;
-    // public function getRoleByName($name);
-    // public function getRoleById($id);
-    // public function getSubRoles($id);
-    // public function getUserRoles($name);
-    // public function createGroup($group);
-    // public function getUserGroup($userId);
-    // public function addUserToGroup($userId, $groupId);
-    // public function addUserToRole($userId, $roleId);
-    // public function addRoleToGroup($roleId, $groupId);
-    // public function removeUserFromGroup($userId, $groupId);
-    // public function removeRoleFromGroup($userId, $roleId);
-    // public function showUserPermissions($userId);
+    public function getRoles(): array;
+    public function getRoleByName($name): array;
+    public function getRoleUsers($name): array;
+    public function createGroup(Group $group): array;
+    public function getGroups($q='', $limit = 20, $offset = 0): array;
+    public function getGroupUsers($limit = 20, $offset = 0): array;
+    public function getUserGroups($q='', $limit = 20, $offset = 0): array;
+    public function addUserToGroup($userId, $groupId): array;
+    public function addRoleToGroup($roleId, $groupId): array;
+    public function getGroupRoles($limit = 20, $offset = 0): array;
+    // public function removeUserFromGroup($userId, $groupId): array;
+    // public function removeRoleFromGroup($userId, $roleId): array;
+    // public function showUserPermissions($userId): array;
 }
