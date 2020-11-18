e3eb0265c559f2836fa5aee51ffa0b5b6ee29de1
Håkon <38793364+hakonlarsson@users.noreply.github.com>
2020-11-18 14:30:18 +0000

    CON-1018 Commendated requested changes by PR code review

--- tests/Unit/OrganizationTest.php
+++ tests/Unit/OrganizationTest.php
@@ -18,18 +18,27 @@ class OrganizationTest extends TestCase
     }
 
     /**
-     * @covers test create an organization and make sure an uuid is created
-     * and name is created
+     * @covers test create a basic organization with an uuid and name
      */
-    public function test_create_organization() {
-        $event = Event::fake();
+    public function test_create_a_basic_organization() {
+        // prevent firing events
+        Event::fake();
 
         $org = factory(Organization::class)->create();
+        $org = Organization::findOrFail($org->id);
         $this->assertTrue(strlen($org->name) > 0,
             "Organization doesn't have a valid name");
         $this->assertStringMatchesFormat("%x-%x-%x-%x-%x", $org->uuid,
             "Organization doesn't have a valid uuid");
+    }
 
+    /**
+     * @covers test create an organization and make sure an OrganizationCreatedEvent is sent
+     */
+    public function test_create_organization_and_validate_OrganizationCreatedEvent_is_sent() {
+        $event = Event::fake();
+
+        $org = factory(Organization::class)->create();
         $event->assertDispatched(OrganizationCreatedEvent::class,
             function(OrganizationCreatedEvent $event) use ($org) {
                 return $org->uuid == $event->organization->uuid;
