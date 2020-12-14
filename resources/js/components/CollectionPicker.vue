c87980431de8d576f5c85b958201b0418aff3789
Ulf Thorsen <ulf.thorsen@piql.com>
2020-12-14 09:38:26 +0000

    Added translatin for 'Your collection'

--- resources/js/components/CollectionPicker.vue
+++ resources/js/components/CollectionPicker.vue
@@ -69,7 +69,7 @@ export default {
         },
         singleCollectionTitle: {
             type: String,
-            default: "Your Collection"
+            default: $t('Your collection')
         },
         selectionDisabled: {
             type: Boolean,
