50c95b43195c4e28e87e3e54cc7ca1cc596a3546
Ulf Thorsen <ulf.thorsen@piql.com>
2020-12-17 16:07:17 +0000

    Squash refactoring migrations

--- database/migrations/2020_12_17_082435_split_organization_from_account.php
+++ database/migrations/2020_12_17_082435_split_organization_from_account.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RenameArchivesToCollections extends Migration
+class SplitOrganizationFromAccount extends Migration
 {
     /**
      * Run the migrations.
@@ -15,14 +15,32 @@ class RenameArchivesToCollections extends Migration
      */
     public function up()
     {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid("uuid")->unique();
+            $table->string("name");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('account_uuid', 'organization_uuid');
+        });
+
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->uuid('organization_uuid');
+        });
+
         Schema::table('holdings', function (Blueprint $table) {
             $table->renameColumn('owner_archive_uuid', 'collection_uuid');
             $table->dropColumn('parent_id');
             $table->dropColumn('position');
         });
+
         Schema::table('storage_properties', function (Blueprint $table) {
             $table->renameColumn('archive_uuid', 'collection_uuid');
         });
+
         Schema::rename('archives', 'collections');
         Schema::rename('accounts', 'archives');
         Schema::table('collections', function (Blueprint $table) {
@@ -41,6 +59,7 @@ class RenameArchivesToCollections extends Migration
             "updated_at" => $creationTime->format('y-m-d H:i:s'),
             "created_at" => $creationTime->format('y-m-d H:i:s')
         ]);
+
         DB::table('archives')
             ->update(["organization_uuid" => $organizationUuid]);
         DB::table('users')
@@ -61,15 +80,36 @@ class RenameArchivesToCollections extends Migration
         Schema::table('collections', function (Blueprint $table) {
             $table->renameColumn('archive_uuid', 'account_uuid');
         });
+
         Schema::rename('archives', 'accounts');
+
         Schema::rename('collections', 'archives');
+
         Schema::table('storage_properties', function (Blueprint $table) {
             $table->renameColumn('collection_uuid', 'archive_uuid');
         });
+
         Schema::table('holdings', function (Blueprint $table) {
             $table->renameColumn('collection_uuid', 'owner_archive_uuid');
             $table->bigInteger('parent_id')->nullable()->default(null);
             $table->integer('position')->default(0);
         });
+
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('organization_uuid');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('organization_uuid', 'account_uuid');
+        });
+
+        if(DB::table('accounts')->count() > 0)
+        {
+            $accountUuid = DB::table('accounts')->first()->uuid;
+            DB::table('users')->update(["account_uuid" => $accountUuid]);
+        }
+
+        Schema::dropIfExists('organizations');
+
     }
 }
